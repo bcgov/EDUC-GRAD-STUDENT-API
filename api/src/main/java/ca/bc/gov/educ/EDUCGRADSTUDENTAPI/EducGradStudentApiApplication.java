3f7fdd5456dfa1c7a1ea4b03a1465d906c557432
Kamal Mohammed <kamal.mohammed@outlook.com>
2020-10-14 01:52:22 -0700

    Initial commit

---
+++ api/src/main/java/ca/bc/gov/educ/EDUCGRADSTUDENTAPI/EducGradStudentApiApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.EDUCGRADSTUDENTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducGradStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducGradStudentApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/ca/bc/gov/educ/EDUCGRADSTUDENTAPI/EducGradStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.EDUCGRADSTUDENTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducGradStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
