15b00e6fee6ad838de1774f506001d5c51a3f1db
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-08-10 16:07:42 -0700

    Date time related changes

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/entity/BaseEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/entity/BaseEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import ca.bc.gov.educ.api.gradstudent.util.EducGradStudentApiConstants;
 import lombok.Data;
@@ -20,15 +21,18 @@ public class BaseEntity {
 	@Column(name = "CREATE_USER", nullable = true)
     private String createUser;
 	
-	@Column(name = "CREATE_DATE", nullable = true)
+	@Column(name = "CREATE_DATE", columnDefinition="datetime",nullable = false)
 	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss")
     private Date createDate;
 	
-	@Column(name = "UPDATE_USER", nullable = true)
+	
+	@Column(name = "UPDATE_USER", nullable = false)
     private String updateUser;
 	
-	@Column(name = "UPDATE_DATE", nullable = true)
+	@Column(name = "UPDATE_DATE", columnDefinition="datetime",nullable = true)
 	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss")
 	private Date updateDate;
 	
 	@PrePersist
@@ -46,7 +50,7 @@ public class BaseEntity {
 
 	@PreUpdate
 	protected void onPersist() {
-		this.updateDate = new Date(System.currentTimeMillis());
+		this.updateDate = new Date();
 		if (StringUtils.isBlank(updateUser)) {
 			this.updateUser = EducGradStudentApiConstants.DEFAULT_UPDATED_BY;
 		}
@@ -54,7 +58,7 @@ public class BaseEntity {
 			this.createUser = EducGradStudentApiConstants.DEFAULT_CREATED_BY;
 		}
 		if (this.createDate == null) {
-			this.createDate = new Date(System.currentTimeMillis());
+			this.createDate = new Date();
 		}
 	}
 }
