154db5eeccb860c18dab30fa6e8325e2f38f2745
Jinil Sung <infstar@gmail.com>
2021-10-08 10:38:40 -0700

    Enhanced Event table to have more traceability on NATS messaging.
    Enhanced Event table to have more traceability on NATS messaging.

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/GraduationStatusService.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/GraduationStatusService.java
@@ -143,14 +143,14 @@ public class GraduationStatusService {
             historyService.createStudentHistory(gradEntity, "GRADALG");
             final GraduationStudentRecord savedGraduationStatus = graduationStatusTransformer.transformToDTO(gradEntity);
             final GradStatusEvent gradStatusEvent = createGradStatusEvent(gradEntity.getCreateUser(), gradEntity.getUpdateUser(),
-                    savedGraduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, accessToken);
+                    savedGraduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, "GRADALG", accessToken);
             gradStatusEventRepository.save(gradStatusEvent);
             return Pair.of(savedGraduationStatus, gradStatusEvent);
         } else {
             sourceObject = graduationStatusRepository.saveAndFlush(sourceObject);
             final GraduationStudentRecord savedGraduationStatus = graduationStatusTransformer.transformToDTO(sourceObject);
             final GradStatusEvent gradStatusEvent = createGradStatusEvent(sourceObject.getCreateUser(), sourceObject.getUpdateUser(),
-                    savedGraduationStatus, EventType.CREATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_CREATED, accessToken);
+                    savedGraduationStatus, EventType.CREATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_CREATED, "GRADALG", accessToken);
             gradStatusEventRepository.save(gradStatusEvent);
             return Pair.of(savedGraduationStatus, gradStatusEvent);
         }
@@ -181,7 +181,7 @@ public class GraduationStatusService {
             historyService.createStudentHistory(gradEntity, "USEREDIT");
             final GraduationStudentRecord updatedGraduationStatus = graduationStatusTransformer.transformToDTO(gradEntity);
             final GradStatusEvent gradStatusEvent = createGradStatusEvent(gradEntity.getCreateUser(), gradEntity.getUpdateUser(),
-                    updatedGraduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, accessToken);
+                    updatedGraduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, "USEREDIT", accessToken);
             gradStatusEventRepository.save(gradStatusEvent);
             return Pair.of(updatedGraduationStatus, gradStatusEvent);
         } else {
@@ -468,7 +468,7 @@ public class GraduationStatusService {
                     historyService.createStudentHistory(gradEntity, "USERUNGRAD");
                     final GraduationStudentRecord graduationStatus = graduationStatusTransformer.transformToDTO(gradEntity);
                     final GradStatusEvent gradStatusEvent = createGradStatusEvent(gradEntity.getCreateUser(), gradEntity.getUpdateUser(),
-                            graduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, accessToken);
+                            graduationStatus, EventType.UPDATE_GRAD_STATUS, EventOutcome.GRAD_STATUS_UPDATED, "USERUNGRAD", accessToken);
                     gradStatusEventRepository.save(gradStatusEvent);
                     return Pair.of(graduationStatus, gradStatusEvent);
 		        } else {
@@ -500,6 +500,7 @@ public class GraduationStatusService {
     private GradStatusEvent createGradStatusEvent(String createUser, String updateUser,
                                                   GraduationStudentRecord graduationStatus,
                                                   EventType eventType, EventOutcome eventOutcome,
+                                                  String activityCode,
                                                   String accessToken) throws JsonProcessingException {
         if (StringUtils.isBlank(graduationStatus.getPen())) {
             GradSearchStudent gradSearchStudent = gradStudentService.getStudentByStudentIDFromStudentAPI(graduationStatus.getStudentID().toString(), accessToken);
@@ -517,6 +518,7 @@ public class GraduationStatusService {
                 .eventType(eventType.toString())
                 .eventStatus(DB_COMMITTED.toString())
                 .eventOutcome(eventOutcome.toString())
+                .activityCode(activityCode)
                 .build();
     }
 
