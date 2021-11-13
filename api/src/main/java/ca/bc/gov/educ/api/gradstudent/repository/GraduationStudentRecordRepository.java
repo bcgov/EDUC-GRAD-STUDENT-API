99a1abd1ab6f3c0d937b65bcaf699d11af5f4d7b
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2021-11-12 17:09:23 -0800

    Resillience and Student Status change

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/repository/GraduationStudentRecordRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/repository/GraduationStudentRecordRepository.java
@@ -17,6 +17,7 @@ public interface GraduationStudentRecordRepository extends JpaRepository<Graduat
     List<GraduationStudentRecordEntity> findAll();
 
 	List<GraduationStudentRecordEntity> findByRecalculateGradStatus(String recalulateFlag);
+	List<GraduationStudentRecordEntity> findByStudentStatus(String studentStatus);
 	GraduationStudentRecordEntity findByStudentID(UUID studentID);
 
 	@Query("select c from GraduationStudentRecordEntity c where c.studentStatus=:statusCode")
