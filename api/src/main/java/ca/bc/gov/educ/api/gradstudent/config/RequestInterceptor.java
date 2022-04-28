339250e7e7df129873b8426c2d7ab4c39766460c
Jinil Sung <infstar@gmail.com>
2022-04-28 14:42:08 -0700

    username is saved in transaction & correlationID is logged into Splunk.
    username is saved in transaction & correlationID is logged into Splunk.

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/config/RequestInterceptor.java
@@ -3,10 +3,14 @@ package ca.bc.gov.educ.api.gradstudent.config;
 import ca.bc.gov.educ.api.gradstudent.util.EducGradStudentApiConstants;
 import ca.bc.gov.educ.api.gradstudent.util.GradValidation;
 import ca.bc.gov.educ.api.gradstudent.util.LogHelper;
+import ca.bc.gov.educ.api.gradstudent.util.ThreadLocalStateUtil;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
@@ -32,6 +36,19 @@ public class RequestInterceptor implements AsyncHandlerInterceptor {
 			request.setAttribute("startTime", startTime);
 		}
 		validation.clear();
+		// correlationID
+		val correlationID = request.getHeader(EducGradStudentApiConstants.CORRELATION_ID);
+		if (correlationID != null) {
+			ThreadLocalStateUtil.setCorrelationID(correlationID);
+		}
+
+		// username
+		JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+		Jwt jwt = (Jwt) authenticationToken.getCredentials();
+		String email = (String) jwt.getClaims().get("email");
+		if (email != null) {
+			ThreadLocalStateUtil.setCurrentUser(email);
+		}
 		return true;
 	}
 
@@ -49,6 +66,9 @@ public class RequestInterceptor implements AsyncHandlerInterceptor {
 		val correlationID = request.getHeader(EducGradStudentApiConstants.CORRELATION_ID);
 		if (correlationID != null) {
 			response.setHeader(EducGradStudentApiConstants.CORRELATION_ID, request.getHeader(EducGradStudentApiConstants.CORRELATION_ID));
+			// reset
+			ThreadLocalStateUtil.setCorrelationID(null);
+			ThreadLocalStateUtil.setCurrentUser(null);
 		}
 	}
 }
