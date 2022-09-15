8cf884701ac0f5e42f9bb70c829d99a237ed83e4
alexryba <alex.rybakov@gov.bc.ca>
2022-09-15 17:59:34 -0600

    GRAD2-1413 Student Verification Report - TVR further modifications

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/entity/GraduationStudentRecordEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/entity/GraduationStudentRecordEntity.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -80,4 +81,8 @@ public class GraduationStudentRecordEntity extends BaseEntity {
 
     @Transient
     private String legalLastName;
+
+    @OneToMany
+    @JoinColumn(name = "GRADUATION_STUDENT_RECORD_ID", insertable = false, updatable = false)
+    private List<StudentCareerProgramEntity> careerPrograms;
 }
