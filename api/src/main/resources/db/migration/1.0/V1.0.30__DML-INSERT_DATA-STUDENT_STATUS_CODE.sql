c3bd00c37422ff4fac5d7399aeda7ba790d9263a
Xiaozhen Niu <xiaozhen.niu@cgi.com>
2022-09-28 13:54:57 -0700

    GRAD2-1524, add a new code into student status code table

---
+++ api/src/main/resources/db/migration/1.0/V1.0.30__DML-INSERT_DATA-STUDENT_STATUS_CODE.sql
@@ -0,0 +1,9 @@
+INSERT INTO STUDENT_STATUS_CODE
+(STUDENT_STATUS_CODE,LABEL,DESCRIPTION,DISPLAY_ORDER,EFFECTIVE_DATE,EXPIRY_DATE)
+VALUES (
+'HIS',
+'Archived-Corrected Historical',
+'Indicates that an archived student (ARC) has had a correction made to their data (change or new submission), per the request of the permanent student record holder (i.e., school or district), and an updated transcript and/or a new certificate(s) is needed.',
+60,
+TIMESTAMP'2021-07-12 00:00:00.0',
+NULL);
