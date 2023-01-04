364dbaae06459f856166b7fbbc3ce1a4e7280113
alexryba <alex.rybakov@gov.bc.ca>
2023-01-04 16:46:21 -0700

    GRAD2-1770 Add Latest Session Date to student "CLOB"

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationData.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationData.java
@@ -1,12 +1,14 @@
 package ca.bc.gov.educ.api.gradstudent.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Data
@@ -19,4 +21,13 @@ public class GraduationData {
     private List<GradRequirement> nonGradReasons;
     private boolean dualDogwood;
     private boolean isGraduated;
+    private String latestSessionDate;
+
+    @JsonIgnore
+    public long getSessionDateMonthsIntervalNow() {
+        if(StringUtils.isBlank(latestSessionDate)) {
+            return Integer.MAX_VALUE;
+        }
+        return ChronoUnit.MONTHS.between(LocalDate.parse(latestSessionDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")), LocalDate.now());
+    }
 }
