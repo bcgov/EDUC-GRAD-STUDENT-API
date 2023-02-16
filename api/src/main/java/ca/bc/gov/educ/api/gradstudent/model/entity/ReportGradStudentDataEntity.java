f2053e44a132eca764a4c0940bbbdccbf4aabb2b
alexryba <alex.rybakov@gov.bc.ca>
2023-02-15 17:06:43 -0700

    Migrate new code to 3.02

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/entity/ReportGradStudentDataEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/entity/ReportGradStudentDataEntity.java
@@ -1,11 +1,11 @@
 package ca.bc.gov.educ.api.gradstudent.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.UUID;
 
