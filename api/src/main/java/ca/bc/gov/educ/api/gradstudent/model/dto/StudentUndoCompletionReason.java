01b774b32e2917bead345893d57507a27a30e69d
alexryba <alex.rybakov@gov.bc.ca>
2023-04-24 12:21:56 -0600

    GRADT-644 Student Graduation API should return timestamps in ISO standard

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/StudentUndoCompletionReason.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/StudentUndoCompletionReason.java
@@ -1,9 +1,11 @@
 package ca.bc.gov.educ.api.gradstudent.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -15,4 +17,8 @@ public class StudentUndoCompletionReason extends BaseModel {
     private UUID graduationStudentRecordID;
     private String undoCompletionReasonCode;
     private String undoCompletionReasonDescription;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date createDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date updateDate;
 }
