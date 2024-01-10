846c81b6b58341d8cf5eae1d8993f314b7ce89f6
arybakov <alex.rybakov@cgi.com>
2024-01-10 14:13:38 -0700

    GRAD2-2377 BATCH - HD-22859-GRAD - P3 Monthly, Supplemental and YE Distribution runs should direct Certificates to School at Graduation, not School of Record

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationStudentRecordDistribution.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationStudentRecordDistribution.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Component
-public class GraduationStudentRecordDistribution extends BaseModel{
+public class GraduationStudentRecordDistribution extends BaseModel {
 
     private UUID studentID;
     private String pen;
@@ -18,6 +18,7 @@ public class GraduationStudentRecordDistribution extends BaseModel{
     private String legalMiddleNames;
     private String legalLastName;
     private String schoolOfRecord;
+    private String schoolAtGrad;
     private String programCompletionDate;
     private String honoursStanding;
     private String program;
