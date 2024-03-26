eeb8c80549c6bf31c5a47d3c72b0b1d56a371c01
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-03-26 14:56:46 -0700

    Removed unused configmap entry

--- tools/config/override-configmap-dev.sh
+++ tools/config/override-configmap-dev.sh
@@ -60,8 +60,7 @@ oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
   --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
-oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
   --from-literal=parsers.conf="$PARSER_CONFIG" \
-  --from-literal=CRON_SCHEDULED_REFRESH_NON_GRAD_STATUS="0 */10 * ? * *"
   --dry-run=client -o yaml | oc apply -f -
