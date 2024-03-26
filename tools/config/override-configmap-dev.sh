a33c1bd571efd0e1afe0d637f36974852a368762
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-03-26 14:58:45 -0700

    Fixed env value

--- tools/config/override-configmap-dev.sh
+++ tools/config/override-configmap-dev.sh
@@ -60,7 +60,7 @@ oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
   --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
-oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
   --from-literal=parsers.conf="$PARSER_CONFIG" \
   --dry-run=client -o yaml | oc apply -f -
