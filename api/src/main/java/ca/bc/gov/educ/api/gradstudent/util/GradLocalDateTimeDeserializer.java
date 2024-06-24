68f573e3b02e01357a7e7326f067a1e2a906fb04
arybakov <alex.rybakov@cgi.com>
2024-06-24 12:14:41 -0600

    GRAD2-2799-P3 GRAD Incident: REGALG is creating SCCP certificates for students with future completion dates- edit summary P3 GRAD Incident: REGALG is creating SCCP certificates for students with future completion dates

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/util/GradLocalDateTimeDeserializer.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/util/GradLocalDateTimeDeserializer.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 import static ca.bc.gov.educ.api.gradstudent.util.EducGradStudentApiConstants.*;
 
@@ -41,7 +42,7 @@ public class GradLocalDateTimeDeserializer extends StdDeserializer<LocalDateTime
             if(slashCount > 0) {
                 formatter = DateTimeFormatter.ofPattern(SECOND_DEFAULT_DATE_FORMAT);
             }
-            return LocalDateTime.parse(dateAsString, formatter);
+            return LocalDateTime.parse(dateAsString, formatter).with(TemporalAdjusters.lastDayOfMonth());
         } else if(jsonParser.hasToken(JsonToken.VALUE_NUMBER_INT)) {
             long timestamp = jsonParser.getValueAsLong();
             return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
