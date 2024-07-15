fb727654eea479c2ca19291a47896bc3707c397e
arybakov <alex.rybakov@cgi.com>
2024-07-15 12:04:00 -0600

    GRAD2-2283 Student Archive Process - Backend endpoints To COMPLETE

--- api/src/test/java/ca/bc/gov/educ/api/gradstudent/service/GraduationStatusServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/gradstudent/service/GraduationStatusServiceTest.java
@@ -2904,16 +2904,15 @@ public class GraduationStatusServiceTest {
 
     @Test
     public void testArchiveStudents() {
-        Mockito.when(graduationStatusRepository.archiveStudents("12345678", "CUR", "ARC", 1L)).thenReturn(1);
         Mockito.when(graduationStatusRepository.archiveStudents(List.of("12345678"), "CUR", "ARC", 1L)).thenReturn(1);
         Integer count = graduationStatusService.archiveStudents(1L, List.of("12345678"), "CUR");
         assertThat(count).isNotNull().isEqualTo(1);
     }
 
     @Test
-    public void testArchiveStudentUpdates() {
-        Mockito.when(graduationStatusRepository.archiveStudents(List.of("12345678"), "CUR", "ARC", 1L)).thenReturn(1);
-        Integer count = graduationStatusService.archiveStudents(1L, List.of("12345678"), "CUR");
+    public void testArchiveStudentEmpty() {
+        Mockito.when(graduationStatusRepository.archiveStudents("CUR", "ARC", 1L)).thenReturn(1);
+        Integer count = graduationStatusService.archiveStudents(1L, new ArrayList<>(), "CUR");
         assertThat(count).isNotNull().isEqualTo(1);
     }
 
