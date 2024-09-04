c8b27f901f6ef62b9243625f567538e7bf6cf869
arybakov <alex.rybakov@cgi.com>
2024-09-03 20:12:32 -0600

    Performance

--- api/src/test/java/ca/bc/gov/educ/api/gradstudent/service/HistoryServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/gradstudent/service/HistoryServiceTest.java
@@ -318,6 +318,7 @@ public class HistoryServiceTest {
         when(graduationStudentRecordRepository.updateGraduationStudentRecordEntitiesBatchIdWhereStudentIDsIn(4000L, List.of(studentID))).thenReturn(1);
         when(graduationStudentRecordHistoryRepository.insertGraduationStudentRecordHistoryByBatchIdAndStudentIDs(4000L, List.of(studentID), "activityCode", "USER")).thenReturn(1);
         when(graduationStudentRecordHistoryRepository.insertGraduationStudentRecordHistoryByBatchId(4000L, "activityCode", "USER")).thenReturn(1);
+        when(graduationStudentRecordHistoryRepository.insertGraduationStudentRecordHistoryByBatchId(4000L, "USERSTUDARC", "USER")).thenReturn(1);
 
         var result = historyService.updateStudentRecordHistoryDistributionRun(4000L, "USER", "activityCode", List.of(studentID));
         assertThat(result).isNotNull().isEqualTo(1);
