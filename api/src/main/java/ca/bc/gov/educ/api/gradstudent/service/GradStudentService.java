61690d74d8ecbdb222db5952b7c1c70b528b5ef2
Alexander Rybakov <83988488+arybakov-cgi@users.noreply.github.com>
2024-09-05 09:39:55 -0600

    Added debug logging (#680)

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/GradStudentService.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/GradStudentService.java
@@ -408,21 +408,14 @@ public class GradStudentService {
 
 	public List<UUID> getStudentIDsBySearchCriteriaOrAll(StudentSearchRequest searchRequest) {
 		ArrayList<UUID> result = new ArrayList<>();
-		boolean paramsNotEmpty = false;
 		if(searchRequest.getStudentIDs() != null && !searchRequest.getStudentIDs().isEmpty()) {
-			paramsNotEmpty = true;
 			result.addAll(searchRequest.getStudentIDs());
 		}
 		if(searchRequest.getPens() != null && !searchRequest.getPens().isEmpty()) {
-			paramsNotEmpty = true;
 			result.addAll(graduationStatusRepository.findStudentIDsByPenIn(searchRequest.getPens()));
 		}
 		if(searchRequest.getSchoolOfRecords() != null && !searchRequest.getSchoolOfRecords().isEmpty()) {
-			paramsNotEmpty = true;
 			result.addAll(graduationStatusRepository.findBySchoolOfRecordIn(searchRequest.getSchoolOfRecords()));
-		}
-		if(!paramsNotEmpty) {
-			result.addAll(graduationStatusRepository.findAllStudentGuids());
 		}
 		return result;
 	}
