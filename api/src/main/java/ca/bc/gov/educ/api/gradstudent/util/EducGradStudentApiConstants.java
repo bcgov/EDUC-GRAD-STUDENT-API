767eb4d92cde5a5299478e46a1ff306544581b0c
mightycox <j.acox@hotmail.com>
2024-10-30 10:04:51 -0700

    GRAD2-3018 - Updates from pr review and requirement changes

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/util/EducGradStudentApiConstants.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/util/EducGradStudentApiConstants.java
@@ -43,7 +43,7 @@ public class EducGradStudentApiConstants {
     public static final String GRADUATION_RECORD_HISTORY_BY_BATCH_ID_DISTRIBUTION_RUN = "/distribution/batchid/{batchID}";
     public static final String UPDATE_GRAD_STUDENT_FLAG_BY_BATCH_JOB_TYPE_AND_MULTIPLE_STUDENTIDS = "/multistudentids/batchflag/jobtype/{batchJobType}";
     public static final String GRAD_STUDENT_NON_GRAD_REASON_BY_PEN = "/pen/{pen}/nongrad-reason";
-    public static final String GET_ALL_STUDENT_GRADE_CODES = "/student-grade-codes";
+    public static final String GET_ALL_STUDENT_GRADE_CODES = "/grade-codes";
 
     public static final String GRAD_STUDENT_OPTIONAL_PROGRAM_BY_PEN = "/optionalprogram/studentid/{studentID}";
     public static final String GRAD_STUDENT_OPTIONAL_PROGRAM_BY_PEN_PROGRAM_OPTIONAL_PROGRAM = "/optionalprogram/{studentID}/{optionalProgramID}";
