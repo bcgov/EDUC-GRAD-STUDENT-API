0f768876cd4b9d96695e5fa8c056445b7835a24f
Erin Eckerman <erineckerman@gmail.com>
2025-05-07 12:38:48 -0700

    Add grouping by school ID

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationCountProjection.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/model/dto/GraduationCountProjection.java
@@ -1,6 +1,8 @@
 package ca.bc.gov.educ.api.gradstudent.model.dto;
 
 public interface GraduationCountProjection {
+    String getSchoolOfRecordId();
+    
     Long getCurrentGraduates();
 
     Long getCurrentNonGraduates();
