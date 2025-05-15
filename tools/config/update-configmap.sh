344c849da2733686c39d58c75ffffd04ac473b54
cditcher <chris.ditcher@gov.bc.ca>
2025-05-15 13:33:00 -0700

    Remove sql logging

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -67,7 +67,7 @@ oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
   --from-literal=PEN_API="http://student-api-master.$COMMON_NAMESPACE-$envValue.svc.cluster.local:8080/" \
   --from-literal=CRON_SCHEDULED_REFRESH_NON_GRAD_STATUS="0 0 0 1 * ?" \
   --from-literal=ENABLE_COMPRESSION="true" \
-  --from-literal=SHOW_SQL_LOGS="true" \
+  --from-literal=SHOW_SQL_LOGS="false" \
   --from-literal=MAX_LIFETIME="120000" \
   --dry-run=client -o yaml | oc apply -f -
 
