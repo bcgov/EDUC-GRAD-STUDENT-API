fde62f0b3345e09bb454ebb5ceb6d93fbe238344
githubmamatha <harmainimamatha@gmail.com>
2025-09-04 17:34:22 -0700

    Set the activity code for REGALG

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/repository/GraduationStudentRecordSearchSpecification.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/repository/GraduationStudentRecordSearchSpecification.java
@@ -41,7 +41,7 @@ public class GraduationStudentRecordSearchSpecification implements Specification
                     curStatusOptional
             );
         }
-        boolean certDist = StringUtils.equalsAnyIgnoreCase(searchCriteria.activityCode, "USERDISTOC", "USERDISTRC");
+        boolean certDist = StringUtils.equalsAnyIgnoreCase(searchCriteria.activityCode, "GRADALG", "USERDISTOC", "USERDISTRC");
         if(certDist) {
             curStatusOptional = criteriaBuilder.not(root.get(STUDENT_STATUS).in("MER", "DEC"));
         } else {
@@ -50,7 +50,7 @@ public class GraduationStudentRecordSearchSpecification implements Specification
 
         Predicate datesRangePredicate = criteriaBuilder.and();
         if(searchCriteria.getGradDateFrom() != null && searchCriteria.getGradDateTo() != null) {
-            boolean transcriptDist = StringUtils.equalsAnyIgnoreCase(searchCriteria.activityCode, "USERDISTOT", "USERDISTRT");
+            boolean transcriptDist = StringUtils.equalsAnyIgnoreCase(searchCriteria.activityCode,  "USERDISTOT", "USERDISTRT");
             if(transcriptDist) {
                 curStatusOptional = criteriaBuilder.not(root.get(STUDENT_STATUS).in("MER", "DEC"));
             }
