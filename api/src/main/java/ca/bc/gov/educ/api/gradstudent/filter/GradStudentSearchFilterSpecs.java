730d875552012c3180c1e954686fba5ca8d68592
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-12-12 10:36:25 -0800

    compilation issue

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/filter/GradStudentSearchFilterSpecs.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/filter/GradStudentSearchFilterSpecs.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Service;
 
 import java.time.chrono.ChronoLocalDate;
 import java.time.chrono.ChronoLocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @Service
 @Slf4j
 public class GradStudentSearchFilterSpecs extends BaseFilterSpecs<GradStudentSearchDataEntity> {
 
-  public GradStudentSearchFilterSpecs(FilterSpecifications<GradStudentSearchDataEntity, ChronoLocalDate> dateFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, ChronoLocalDateTime<?>> dateTimeFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Integer> integerFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, String> stringFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Long> longFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, UUID> uuidFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Boolean> booleanFilterSpecifications, Converters converters) {
-    super(dateFilterSpecifications, dateTimeFilterSpecifications, integerFilterSpecifications, stringFilterSpecifications, longFilterSpecifications, uuidFilterSpecifications, booleanFilterSpecifications, converters);
+  public GradStudentSearchFilterSpecs(FilterSpecifications<GradStudentSearchDataEntity, ChronoLocalDate> dateFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, ChronoLocalDateTime<?>> dateTimeFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Integer> integerFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, String> stringFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Long> longFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, UUID> uuidFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Boolean> booleanFilterSpecifications, FilterSpecifications<GradStudentSearchDataEntity, Date> utilDateFilterSpecifications, Converters converters) {
+    super(dateFilterSpecifications, dateTimeFilterSpecifications, integerFilterSpecifications, stringFilterSpecifications, longFilterSpecifications, uuidFilterSpecifications, booleanFilterSpecifications, utilDateFilterSpecifications, converters);
   }
 
 }
