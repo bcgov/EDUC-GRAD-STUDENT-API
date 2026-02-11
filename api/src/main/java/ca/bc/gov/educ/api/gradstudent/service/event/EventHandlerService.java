cd0acc8c2450856728f7b1e6732dbc90af20d25e
arcshiftsolutions <arcshiftsolutions@gmail.com>
2026-02-11 11:29:35 -0800

    Added logging

--- api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/event/EventHandlerService.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstudent/service/event/EventHandlerService.java
@@ -230,17 +230,19 @@ public class EventHandlerService {
                 .toList();
 
         // Process in batches
+        log.info("Setting flags for students in batch");
         int batchSize = 1000;
         for (int i = 0; i < studentIds.size(); i += batchSize) {
             int end = Math.min(i + batchSize, studentIds.size());
             List<UUID> batch = studentIds.subList(i, end);
-
+            
             graduationStudentRecordRepository.updateStudentFlags(batch);
 
             entityManager.flush();
             entityManager.clear();
         }
 
+        log.info("Flags for students in batch");
         event.setEventOutcome(EventOutcome.STUDENT_FLAGS_UPDATED);
         return JsonUtil.getJsonBytesFromObject(event);
     }
